2f9d5224b6bc138f3de9f8c139daea58dc5ad0bc
junevara <junevara1@gmail.com>
2022-08-04 14:54:25 +0200

    Add playRound function to main.js

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random()*(10-1))+1;
+    const randomNumber = Math.floor(Math.random()*(10-1))+1;
     if (randomNumber >= 1 && randomNumber <= 3){
-        return "Rock";
+        return "rock";
     }
     else if (randomNumber >= 4 && randomNumber <= 6){
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-let computerChoice = getComputerChoice();
+const computerChoice = getComputerChoice();
 
 console.log(computerChoice);
+
+const playerChoice = prompt("Chose either Rock, Paper or Scissors!", "Rock");
+const playerChoiceLowerCase= playerChoice.toLowerCase();
+
+function playRound(computerChoice, playerChoice){
+    if (computerChoice === playerChoice){
+        return "draw";
+    }
+    else if (computerChoice === "rock" && playerChoice === "scissors"){
+        return "You lose! Rock beats Scissors!";
+    }
+    else if (computerChoice === "paper" && playerChoice === "rock"){
+        return "You lose! Paper beats Rock!";
+    }
+    else if (computerChoice === "scissors" && playerChoice === "paper"){
+        return "You lose! Scissors beats Paper!"
+    }
+    else { return `You win! ${playerChoice} beats ${computerChoice}!`;
+    }
+}
+
+console.log(playRound(computerChoice, playerChoiceLowerCase));
